31127a7e7e195b791e96eb7173141e9c55ec1d11
ABHINAVX03 <guptaabhinav697@gmail.com>
2026-05-30 03:09:01 +0530

    Allow controlled OTP logging

--- src/main/java/com/codingshuttle/project/uber/uberApp/services/sms/ConsoleSmsProvider.java
+++ src/main/java/com/codingshuttle/project/uber/uberApp/services/sms/ConsoleSmsProvider.java
@@ -2,6 +2,7 @@ package com.codingshuttle.project.uber.uberApp.services.sms;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +15,13 @@ public class ConsoleSmsProvider implements SmsProvider {
 
     private final Environment environment;
 
+    @Value("${app.otp.log-enabled:false}")
+    private boolean otpLogEnabled;
+
     @Override
     public void sendVerificationCode(String phoneNumber, String otp) {
-        if (isDevelopmentMode()) {
-            log.info("[DEV OTP] phoneNumber={} otp={}", phoneNumber, otp);
+        if (otpLogEnabled || isDevelopmentMode()) {
+            log.info("[OTP DEBUG] phoneNumber={} otp={}", phoneNumber, otp);
             return;
         }
         log.warn("No SMS provider is configured. OTP generated for phoneNumber={} but was not sent.", phoneNumber);
